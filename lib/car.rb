9ad792fd6e5a759fdc78605811fd6c945aa4a179
Max MacGillivray <mary.macgillivray@gmail.com>
2022-08-31 10:54:41 -0700

    Added attributes to car, completed iteration 1

--- lib/car.rb
+++ lib/car.rb
@@ -1,15 +1,20 @@
 #Started at 10:20am, 31 August
 
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length
-  def initialize(car_info, monthly_payment, loan_length)
+  attr_reader :make, :model, :monthly_payment, :loan_length, :color
+  def initialize(car_info, monthly_payment, loan_length, color = nil)
     
     @make = car_info.split.first
     @model = car_info.split.last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = color
+
   end
   def total_cost
     @monthly_payment * @loan_length
+  end
+  def paint!(color)
+    @color = color
   end
 end
